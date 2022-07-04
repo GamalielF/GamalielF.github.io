21558b7d69b3c2e225cf4698a1bb183087f8404c
GamalielF <68031782+GamalielF@users.noreply.github.com>
2022-07-04 08:23:40 +0000

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,6 @@
+// ? ----- ----- POPUP ----- -----
+function toogle(){
+	var trailer=document.querySelector(".trailer")
+	var iframe=document.querySelector("iframe")
+	trailer.classList.toggle("active")
+}
